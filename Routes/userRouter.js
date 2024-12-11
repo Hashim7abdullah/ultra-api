4b6d81c7f4f5bb9402bf79090782a97ace90883a
Hashim7abdullah <hashim7abdullah7@gmail.com>
2024-12-11 15:31:32 +0530

    changes made

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { logedUser, registerUser } from '../Controllers/User.js'
+import { forgotPassword, logedUser, registerUser } from '../Controllers/User.js'
 
 const userRouter = express.Router()
 
 userRouter.post("/sign-in", registerUser)
 userRouter.post("/login", logedUser)
+userRouter.post("/forgot-password" , forgotPassword)
 
 export default userRouter
